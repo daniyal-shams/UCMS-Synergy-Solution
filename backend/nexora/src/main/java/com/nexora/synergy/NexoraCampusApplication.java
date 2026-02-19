90689f7206c23c650be3ce9fa81757754e0a83b8
daniyal-shams <hussaindaniyal748@gmail.com>
2026-02-20 04:16:31 +0530

    first commit

---
+++ backend/nexora/src/main/java/com/nexora/synergy/NexoraCampusApplication.java
@@ -0,0 +1,13 @@
+package com.nexora.synergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoraCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexoraCampusApplication.class, args);
+	}
+
+}
